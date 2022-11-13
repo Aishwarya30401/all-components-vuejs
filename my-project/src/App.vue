3007df4f51cb6e156245881cae25e4db169cc2e8
Aishwarya30401 <aishu30401@gmail.com>
2022-11-12 23:19:02 -0800

    components

---
+++ my-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <component></component>
+  </div>
+</template>
+
+<script>
+import Component from './components/Component.vue'
+export default {
+  name: 'App',
+  components: {
+    Component
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
